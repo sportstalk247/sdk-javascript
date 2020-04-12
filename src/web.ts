913951cae3bfcfdb34978d4e514f0b162c3557bf
Ajay <ajayacharya3000@gmail.com>
2020-04-12 17:13:38 +0700

    Make web cleaner

--- src/web.ts
+++ src/web.ts
@@ -1,12 +1,12 @@
 // @ts-ignore
-const client = require('./impl/ChatClient');
-const conversation = require('./impl/ConversationClient');
-const types = require('./models/ChatModels');
-const common = require('./models/CommonModels');
-const conversationtypes = require('./models/ConversationModels');
+const chatClient = require('./impl/ChatClient');
+const conversationClient = require('./impl/ConversationClient');
+const chatModels = require('./models/ChatModels');
+const commonModels = require('./models/CommonModels');
+const conversationModels = require('./models/ConversationModels');
 // @ts-ignore
-var SportsTalkClient = window.SportsTalkClient || client.default || client.ChatClient || client;
-var ConversationClient = window.ConversationClient || conversation.default || conversation.ConversationClient || conversation;
+var SportsTalkClient = window.SportsTalkClient || chatClient.default || chatClient.ChatClient || chatClient;
+var ConversationClient = window.ConversationClient || conversationClient.default || conversationClient.ConversationClient || conversationClient;
 
 if(window) {
     // @ts-ignore
@@ -20,13 +20,13 @@ if(window) {
     // @ts-ignore
     window.SportsTalk.chat = {};
     // @ts-ignore
-    window.SportsTalk.chat.Types = types;
+    window.SportsTalk.chat.Models = chatModels;
     // @ts-ignore
     window.SportsTalk.conversation = {};
     // @ts-ignore
     window.SportsTalk.common = {};
     // @ts-ignore
-    window.SportsTalk.common.Types = common;
+    window.SportsTalk.common.Models = commonModels;
     // @ts-ignore
-    window.SportsTalk.conversation.Types = conversationtypes
+    window.SportsTalk.conversation.Models = conversationModels
 }
