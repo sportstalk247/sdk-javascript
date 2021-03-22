4cbf66b35dd2a8dc931f5696b35efcdc1ea26a58
Ajay <ajayacharya3000@gmail.com>
2021-03-22 15:08:06 +0700

    Self executing closure to wrap API in web

--- src/web.ts
+++ src/web.ts
@@ -1,33 +1,35 @@
 // @Sportstalk Client 3.7.x
-const chatClient = require('./impl/ChatClient');
-const commentClient = require('./impl/CommentClient');
-const chatModels = require('./models/ChatModels');
-const commonModels = require('./models/CommonModels');
-const conversationModels = require('./models/CommentsModels');
-// @ts-ignore
-var ChatClient = window.ChatClient || chatClient.default || chatClient.ChatClient || chatClient;
-var CommentClient = window.CommentClient || commentClient.default || commentClient.CommentClient || commentClient;
-
-if(window) {
-    // @ts-ignore
-    window.ChatClient = ChatClient;
-    // @ts-ignore
-    window.CommentClient = CommentClient;
-    // @ts-ignore
-    window.SportsTalk = {};
-    // @ts-ignore
-    window.Conversation = {}
-    // @ts-ignore
-    window.SportsTalk.chat = {};
-    // @ts-ignore
-    window.SportsTalk.chat.Models = chatModels;
-    // @ts-ignore
-    window.SportsTalk.comments = {};
-    // @ts-ignore
-    window.SportsTalk.common = {};
-    // @ts-ignore
-    window.SportsTalk.common.Models = commonModels;
-    // @ts-ignore
-    window.SportsTalk.comments.Models = conversationModels
-}
-console.log("Chat and commenting powered by Sportstalk247")
+(function(window) {
+    const chatClient = require('./impl/ChatClient');
+    const commentClient = require('./impl/CommentClient');
+    const chatModels = require('./models/ChatModels');
+    const commonModels = require('./models/CommonModels');
+    const conversationModels = require('./models/CommentsModels');
+    if (window) {
+        // @ts-ignore
+        var ChatClient = window.ChatClient || chatClient.default || chatClient.ChatClient || chatClient;
+        // @ts-ignore
+        var CommentClient = window.CommentClient || commentClient.default || commentClient.CommentClient || commentClient;
+        // @ts-ignore
+        window.ChatClient = ChatClient;
+        // @ts-ignore
+        window.CommentClient = CommentClient;
+        // @ts-ignore
+        window.SportsTalk = {};
+        // @ts-ignore
+        window.Conversation = {}
+        // @ts-ignore
+        window.SportsTalk.chat = {};
+        // @ts-ignore
+        window.SportsTalk.chat.Models = chatModels;
+        // @ts-ignore
+        window.SportsTalk.comments = {};
+        // @ts-ignore
+        window.SportsTalk.common = {};
+        // @ts-ignore
+        window.SportsTalk.common.Models = commonModels;
+        // @ts-ignore
+        window.SportsTalk.comments.Models = conversationModels
+    }
+    console.log("Chat and commenting powered by Sportstalk247")
+})(window);
